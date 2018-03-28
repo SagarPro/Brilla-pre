7874b806a2d100775744e0424a746feb9bb38b13
SagarPro <34785921+SagarPro@users.noreply.github.com>
2018-03-28 11:24:05 +0530

    Update ConfigEmail.java

--- app/src/main/java/com/brightkidmont/brilla/ConfigEmail.java
+++ app/src/main/java/com/brightkidmont/brilla/ConfigEmail.java
@@ -2,7 +2,7 @@ package com.brightkidmont.brilla;
 
 public class ConfigEmail {
 
-    public static final String EMAIL ="cs@brightkidmont.com";
-    public static final String PASSWORD ="alwaysbright123";
+    public static final String EMAIL ="your email";
+    public static final String PASSWORD ="your password";
 
 }
